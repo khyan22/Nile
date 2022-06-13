03c7c2be4b399145669c8865cf2a8eb5a9db6f09
khyan22 <khyanalvarez@gmail.com>
2022-06-13 05:09:33 -0400

    established connection between db and server, fixed model typos

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,7 +7,7 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     id: {
-      type: DataTypes.Integer,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
--- models/index.js
+++ models/index.js
@@ -15,14 +15,14 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(tags, {
-  trough: ProductTag,
+Product.belongsToMany(Tag, {
+  through: ProductTag,
   foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
-  trough: ProductTag,
+Tag.belongsToMany(Product, {
+  through: ProductTag,
   foreignKey: 'tag_id'
 })
 
